8d94d2b67f3104e25847cfaad1b5ce83652e3526
Akanksha Singh <akanksha@Akankshas-MacBook-Pro-2.local>
2023-08-25 15:28:30 +0530

    UNRI2-145: Add css

--- js/ThreeDViewer.js
+++ js/ThreeDViewer.js
@@ -25,7 +25,28 @@ export class ThreeDViewer {
 
     // Add a spin loader.
     var target = document.getElementsByClassName(this.settings.progress_element_classes)[0];
-    var spinner = new Spinner();
+
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 38, // The length of each line
+      width: 17, // The line thickness
+      radius: 45, // The radius of the inner circle
+      scale: 1, // Scales overall size of the spinner
+      corners: 1, // Corner roundness (0..1)
+      speed: 1.6, // Rounds per second
+      rotate: 0, // The rotation offset
+      animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: 'green', // CSS color or array of colors
+      fadeColor: 'lightgreen', // CSS color or array of colors
+      top: '50%', // Top position relative to parent
+      left: '50%', // Left position relative to parent
+      shadow: '0 0 1px transparent', // Box-shadow for the lines
+      zIndex: 2000000000, // The z-index (defaults to 2e9)
+      className: 'spinner', // The CSS class to assign to the spinner
+      position: 'absolute', // Element positioning
+    };
+    var spinner = new Spinner(opts);
 
     this.manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
       spinner.spin(target);
